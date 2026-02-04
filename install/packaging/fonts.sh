ec224c446bf81a4f047c17f47c899776a3b29366
Luke <luke.hogan@hey.com>
2026-02-04 14:52:16 -0700

    Update Waybar language widget English icon
    Use a lowercase 'A' in the Bruno Ace font.
    The nerd font 󰯬 is nice, but it renders a bit small.

--- install/packaging/fonts.sh
+++ install/packaging/fonts.sh
@@ -1,4 +1,6 @@
 # Omarchy logo in a font for Waybar use
+# Bruno Ace font is for the Waybar language widget
 mkdir -p ~/.local/share/fonts
 cp ~/.local/share/omarchy/config/omarchy.ttf ~/.local/share/fonts/
+cp ~/.local/share/omarchy/config/BrunoAce-Regular.ttf ~/.local/share/fonts/
 fc-cache
