afab9edb7cb2f19223d8c1064cd32a1531fb7ec1
Luke Hogan <luke.hogan@hey.com>
2026-03-16 12:17:06 -0600

    Update keyd.sh install script
    Do not attempt to restart keyd service because it has not been created yet

--- install/config/keyd.sh
+++ install/config/keyd.sh
@@ -1,6 +1,3 @@
 # Copy the keyd config files
 sudo mkdir -p /etc/keyd
 sudo cp -R ~/.local/share/omarchy/default/keyd/*.conf /etc/keyd/
-
-# Restart keyd so that changes take effect
-sudo systemctl restart keyd
