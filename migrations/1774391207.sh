d155e58f435f2151d297c15ecdc3268d5553db39
Luke Hogan <luke.hogan@hey.com>
2026-03-24 16:37:40 -0600

    Update makima-removal migration

--- migrations/1774391207.sh
+++ migrations/1774391207.sh
@@ -1,8 +1,8 @@
 # Reverse install/config/makima.sh
-sudo systemctl disable makima 2>/dev/null || true
+sudo systemctl disable --now makima 2>/dev/null || true
 sudo systemctl daemon-reload
-sudo rm /etc/systemd/system/makima.service.d/override.conf
-sudo rmdir /etc/systemd/system/makima.service.d
-rm ~/.config/makima/*.toml
-rmdir ~/.config/makima
+sudo rm /etc/systemd/system/makima.service.d/override.conf 2>/dev/null || true
+sudo rmdir /etc/systemd/system/makima.service.d 2>/dev/null || true
+rm ~/.config/makima/*.toml 2>/dev/null || true
+rmdir ~/.config/makima 2>/dev/null || true
 
