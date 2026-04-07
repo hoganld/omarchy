aade3ec9ec287e2551e421856bf050612fee91b2
Luke Hogan <luke.hogan@hey.com>
2026-04-07 11:33:34 -0600

    New fcitx5 themes: Lumon, Retro-82

---
+++ migrations/1775583214.sh
@@ -0,0 +1,5 @@
+# Link the new fcitx5 themes
+echo "Installing Retro 82 Fcitx5 theme"
+ln -nsf "$OMARCHY_PATH"/themes/retro-82/fcitx5 ~/.local/share/fcitx5/themes/retro-82
+echo "Installing Lumon Fcitx5 theme"
+ln -nsf "$OMARCHY_PATH"/themes/lumon/fcitx5 ~/.local/share/fcitx5/themes/lumon
