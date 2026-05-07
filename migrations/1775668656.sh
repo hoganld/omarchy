12ea56128957411fefb2925d25cad3e404755425
Luke Hogan <luke.hogan@hey.com>
2026-05-07 17:14:03 -0600

    Scripts and keys to use all Hyprland layouts
    - Dwindle
    - Scrolling
    - Master
    - Monocle
    
    This patch changes the XKB key mapping. Importantly, F14 is still
    Meta_L, F15 is now Hyper_R, and Meta_R and Hyper_L are unmapped. This
    frees up F16-F19 for the layout keys.

---
+++ migrations/1775668656.sh
@@ -0,0 +1,14 @@
+echo "Update keyd config"
+sudo cp "$OMARCHY_PATH"/default/keyd/*.conf /etc/keyd/
+if [[ -f /etc/keyd/$(hostname).conf ]]; then
+  sudo sed -i "s/f17/f15/" $(hostname).conf
+fi
+
+echo "Restart keyd daemon"
+sudo systemctl restart keyd
+
+echo "Update XKB config"
+cp "$OMARCHY_PATH"/config/xkb/symbols/kog $HOME/.config/xkb/symbols/kog
+
+echo "XKB updates require system reboot"
+touch $HOME/.local/state/omarchy/reboot-required
